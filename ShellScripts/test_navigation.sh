607c7a94b4991d770f6dd99c79fc5e927f5fefdd
JUNG Myoungki <myoungki.jung@gmail.com>
2018-12-13 19:47:42 +1000

    test navigation with amcl_demo in turtlebot pkg
    test_navigation.sh added and chmod u+x .
    
    loading myMap.ymcl file to amcl node and navigates accurately.

---
+++ ShellScripts/test_navigation.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+SRC_DIR=`pwd`
+TURTLEBOT_GAZEBO_WORLD_FILE=${SRC_DIR}/src/World/WorldN/WorldN.world
+TURTLEBOT_GAZEBO_MAP_FILE=${SRC_DIR}/src/World/WorldN/myMap.yaml
+xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch" &
+sleep 5
+xterm -e " roslaunch turtlebot_gazebo amcl_demo.launch" &
+sleep 5
+xterm -e " roslaunch turtlebot_rviz_launchers view_navigation.launch" 
