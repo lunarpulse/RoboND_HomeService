4e1efbcac0c3061ce559edfa266bb18ac7c1af24
JUNG Myoungki <myoungki.jung@gmail.com>
2018-12-13 13:20:57 +1000

    Init, added worldM, testSLAM script
    worlM contains M shape world
    script added to include the world in the envs and launch it with gslam
    and turtlebot teleop

---
+++ ShellScripts/test_slam.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+SRC_DIR=`pwd`
+TURTLEBOT_GAZEBO_WORLD_FILE=${SRC_DIR}/src/World/WorldM/WorldM.world
+xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch" &
+sleep 5
+xterm -e " rosrun gmapping slam_gmapping" &
+sleep 5
+xterm -e " roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+sleep 5
+xterm -e " roslaunch turtlebot_teleop keyboard_teleop.launch" 
